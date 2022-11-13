47f1015c4e533c024598e70bfe89dae55034012a
greg landrum <greg.landrum@gmail.com>
2022-11-13 07:09:18 +0100

    more librarification

---
+++ lwreg/__init__.py
@@ -0,0 +1,9 @@
+#! /bin/env python
+
+# Copyright (C) 2022 Greg Landrum
+# All rights reserved
+# This file is part of lwreg.
+# The contents are covered by the terms of the MIT license
+# which is included in the file LICENSE, 
+
+from .utils import initdb,register,query,retrieve

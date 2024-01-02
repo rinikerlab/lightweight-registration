236a4be58eadc5b13c826797e9a117f8fe1f447a
jbraun <jessica.braun@phys.chem.ethz.ch>
2024-01-02 09:45:54 +0100

    removing depracted lines

--- lwreg/utils.py
+++ lwreg/utils.py
@@ -43,8 +43,6 @@ _defaultConfig = {
     0,  # toggle registering conformers as well as compound structures
     "numConformerDigits":
     3,  # number of digits to use when hashing conformer coordinates
-    # "hashConformer":
-    # 0,  # the molecule's conformer will be part of the basic identity hash
     "lwregSchema":
     "",  # the schema name to use for the lwreg tables (no effect with sqlite3)
 }
@@ -381,14 +379,6 @@ def hash_mol(mol, escape=None, config=None):
         escape=escape,
         enable_tautomer_hash_v2=_lookupWithDefault(config,
                                                    'useTautomerHashv2'))
-
-    # if _lookupWithDefault(config, 'hashConformer'):
-        # escape = layers.get(RegistrationHash.HashLayer.ESCAPE, '')
-        # if escape:
-            # escape += '|\n'
-        # layers[
-            # RegistrationHash.HashLayer.ESCAPE] = escape + _get_conformer_hash(
-                # mol, _lookupWithDefault(config, "numConformerDigits"))
 
     mhash = RegistrationHash.GetMolHash(layers)
 
@@ -1229,15 +1219,6 @@ def initdb(config=None):
         print("cancelled")
         return False
 
-
-# FORBIDDEN_COMBINATIONS = [{
-    # 'registerConformers': 1,
-    # 'hashConformer': 1
-# }, {
-    # 'dbname': None
-# }]
-
-
 def _check_config(config):
     ''' checks that the configuration is valid and no forbidden combinations are present
         
@@ -1254,12 +1235,3 @@ def _check_config(config):
 
     if config.get("dbtype","sqlite3") not in ('sqlite3', 'postgresql'):
         raise ValueError("Possible values for dbtype are sqlite3 and postgresql")
-
-    """
-    for fc in FORBIDDEN_COMBINATIONS:
-        fc_eval = []
-        for k, v in fc.items():
-            fc_eval.append(_lookupWithDefault(config, k) == v)
-        if all(fc_eval):
-            raise ValueError(f'invalid configuration for combination of {fc}')
-    """

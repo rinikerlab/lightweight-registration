66b74643d1de46cfd2e948472a3145a6723e3e2e
greg landrum <greg.landrum@gmail.com>
2025-01-20 04:45:57 +0100

    cartridge integration pass tests, probably needs a bit more testing

---
+++ lwreg/db_utils.py
@@ -0,0 +1,83 @@
+# Copyright (C) 2022-2025 Greg Landrum and other lwreg contributors
+# All rights reserved
+# This file is part of lwreg.
+# The contents are covered by the terms of the MIT license
+# which is included in the file LICENSE,
+
+from . import utils
+
+import logging
+try:
+    import psycopg2
+except ImportError:
+    psycopg2 = None
+    logging.INFO("psycopg2 not available, RDKit cartridge support disabled")
+
+
+# decorator to disable functions if psycopg2 is not available
+def psycopg2_available(func):
+
+    def pass_through(*args, **kwargs):
+        if psycopg2 is None:
+            raise NotImplementedError("psycopg2 is not available")
+        return func(*args, **kwargs)
+
+    return pass_through
+
+
+@psycopg2_available
+def enable_cartridge(config):
+    conn = utils.connect(config)
+    curs = conn.cursor()
+    curs.execute("create extension if not exists rdkit")
+
+
+@psycopg2_available
+def disable_cartridge(config):
+    conn = utils.connect(config)
+    curs = conn.cursor()
+    curs.execute("drop extension if exists rdkit")
+
+
+@psycopg2_available
+def populate_rdkit_schema(config, force=False):
+    if not force:
+        print("This will destroy any existing information in the rdkit schema")
+        response = input("  are you sure? [yes/no]: ")
+        if response != 'yes':
+            print("cancelled")
+            return False
+
+    enable_cartridge(config)
+    conn = utils.connect(config)
+    curs = conn.cursor()
+    rdkit_schema_name = config.get('rdkit_schema', 'rdk')
+    lwreg_schema_name = config.get('lwregSchema', 'public')
+    if not lwreg_schema_name:
+        lwreg_schema_name = 'public'
+    curs.execute(f"create schema if not exists {rdkit_schema_name}")
+    curs.execute(f"drop table if exists {rdkit_schema_name}.mols cascade")
+    curs.execute(
+        f"select molregno, mol_from_ctab(molblock::cstring,false) m into {rdkit_schema_name}.mols from {lwreg_schema_name}.molblocks"
+    )
+    curs.execute(
+        f'create index {rdkit_schema_name}_molidx on {rdkit_schema_name}.mols using gist(m)'
+    )
+    curs.execute(
+        f'''CREATE OR REPLACE FUNCTION {rdkit_schema_name}_copy_new_mol() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    INSERT INTO
+        {rdkit_schema_name}.mols(molregno,m)
+        VALUES(new.molregno,mol_from_ctab(new.molblock::cstring,false));
+           RETURN new;
+END;
+$BODY$
+language plpgsql;''')
+    curs.execute(
+        f'''CREATE OR REPLACE TRIGGER {lwreg_schema_name}_mol_insert_trigger
+     AFTER INSERT ON {lwreg_schema_name}.molblocks
+     FOR EACH ROW
+     EXECUTE FUNCTION {rdkit_schema_name}_copy_new_mol();''')
+    conn.commit()
+    return True
---
+++ lwreg/test_dbutils.py
@@ -0,0 +1,84 @@
+# Copyright (C) 2025 Greg Landrum and other lwreg contributors
+# All rights reserved
+# This file is part of lwreg.
+# The contents are covered by the terms of the MIT license
+# which is included in the file LICENSE,
+import os
+import pwd
+import unittest
+import sqlite3
+import tempfile
+
+from rdkit import Chem
+
+try:
+    from . import utils
+    from . import db_utils
+except ImportError:
+    import utils
+    import db_utils
+
+try:
+    import psycopg2
+except ImportError:
+    psycopg2 = None
+if psycopg2:
+    # we have the connector for postgresql. Is there a server running?
+    cfg = utils.defaultConfig()
+    cfg['dbname'] = 'lwreg_tests'
+    #cfg['host'] = 'localhost'
+    cfg['dbtype'] = 'postgresql'
+    try:
+        cn = utils.connect(config=cfg)
+    except psycopg2.OperationalError:
+        # server not running
+        psycopg2 = None
+
+
+@unittest.skipIf(psycopg2 is None, "skipping postgresql tests")
+class TestCartridgePgSQL(unittest.TestCase):
+    integrityError = psycopg2.errors.UniqueViolation if psycopg2 else None
+
+    def setUp(self):
+        self._config = utils.defaultConfig()
+        self._config['dbname'] = 'lwreg_tests'
+        self._config['dbtype'] = 'postgresql'
+
+    def baseRegister(self):
+        smis = ('CC[C@H](F)Cl', 'CC[C@@H](F)Cl', 'CCC(F)Cl', 'CC(F)(Cl)C')
+        utils._initdb(config=self._config, confirm=True)
+        for smi in smis:
+            utils.register(smiles=smi, config=self._config)
+        mols = [Chem.MolFromSmiles(x) for x in ('Cc1[nH]ncc1', 'Cc1n[nH]cc1')]
+        for mol in mols:
+            utils.register(mol=mol, config=self._config)
+
+    def testPopulateSchema(self):
+        self.baseRegister()
+        db_utils.populate_rdkit_schema(self._config, force=True)
+        conn = utils.connect(config=self._config)
+        curs = conn.cursor()
+        curs.execute("select count(*) from rdk.mols")
+        self.assertEqual(curs.fetchone()[0], 6)
+        curs = None
+
+        # make sure the trigger is there too:
+        utils.register(smiles='CCC[C@H](F)Cl', config=self._config)
+        curs = conn.cursor()
+        curs.execute("select count(*) from rdk.mols")
+        self.assertEqual(curs.fetchone()[0], 7)
+        curs = None
+
+
+@unittest.skipIf(psycopg2 is None, "skipping postgresql tests")
+class TestCartridgePgSQLWithSchema(TestCartridgePgSQL):
+
+    def setUp(self):
+        self._config = utils.defaultConfig()
+        self._config['dbname'] = 'lwreg_tests'
+        self._config['dbtype'] = 'postgresql'
+        self._config['lwregSchema'] = 'lwreg'
+
+
+if __name__ == '__main__':
+    unittest.main()

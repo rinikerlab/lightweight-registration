d50c0cbd69fab573315597d0607d7f57096b1242
Jessica Braun <104623181+brje01@users.noreply.github.com>
2025-03-11 14:04:44 +0100

    Feat/docs (#80)
    * trying to fix problems with readthedocs autodoc
    
    * changing back the readthedocs.yaml
    
    * added sphinx to get autodocs to work
    
    * trial autodocs fix
    
    * editing source/conf.py
    
    * update source/conf.py
    
    * reversing change in requirements.txt
    
    * updates in requirements.txt
    
    * update in source/conf/.py
    
    * update in source/conf/.py
    
    * adding requirements.txt
    
    * update requirements.txt
    
    * update requirements.txt
    
    * update requirements.txt
    
    * update in source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,8 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'lightweight-registration'
 copyright = '2025, Jessica Braun and Greg Landrum'
